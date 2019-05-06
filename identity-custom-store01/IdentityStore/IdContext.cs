e0f528b9a9c8299cb8991a0269a4db21c46cb1f2
morituriblog <none>
2019-05-06 21:52:02 +0900

    Fix IdContext

--- identity-custom-store01/IdentityStore/IdContext.cs
+++ identity-custom-store01/IdentityStore/IdContext.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace IdentityStore
 {
     public class IdContext : IDisposable
     {
+        CloudTableClient _client;
+
         public IdContext(string connectionString)
         {
+            var account = CloudStorageAccount.Parse(connectionString);
+            _client = account.CreateCloudTableClient();
         }
 
         public void Dispose()
         {
+            _client = null;
         }
     }
 }
